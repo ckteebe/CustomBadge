bac59b1bfea8c9bc27b6e6879b45df9babfbbee7
ckteebe <open@spaulus.com>
2013-06-21 14:35:27 +0300

    Update CustomBadge.m

--- Classes/CustomBadge.m
+++ Classes/CustomBadge.m
@@ -1,7 +1,7 @@
 /*
  CustomBadge.m
  
- *** Description: ***
+*** Description: ***
  With this class you can draw a typical iOS badge indicator with a custom text on any view.
  Please use the allocator customBadgeWithString to create a new badge.
  In this version you can modfiy the color inside the badge (insetColor),
@@ -9,15 +9,15 @@
  tell the class if you want a frame around the badge.
  
  *** License & Copyright ***
- Created by Sascha Marc Paulus www.spaulus.com on 04/2011. Version 2.0
+ Created by Sascha Paulus www.spaulus.com on 04/2011. Version 2.0
  This tiny class can be used for free in private and commercial applications.
  Please feel free to modify, extend or distribution this class. 
  If you modify it: Please send me your modified version of the class.
- A commercial distribution of this class is not allowed.
- 
- I would appreciate if you could refer to my website www.spaulus.com if you use this class.
+ Please do not sell the source code solely and keep this text in
+ your copyright section. Thanks.
  
  If you have any questions please feel free to contact me (open@spaulus.com).
+
  */
 
 
