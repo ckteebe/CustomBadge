b4e9e9c4b291141d8a39203b72697e8784cd6853
ckteebe <open@spaulus.com>
2014-11-14 11:10:45 +0100

    Version 2.0 (Deprecated)

--- Classes/CustomBadge.h
+++ Classes/CustomBadge.h
@@ -1,23 +1,33 @@
 /*
+ 
+ ------------------------------------------------------------------------------------
  CustomBadge.h
+ ------------------------------------------------------------------------------------
+ CustomBadge is an UIView which draws a customizable badge on any other view.
+ ------------------------------------------------------------------------------------
+ *** This version (2.0) is DEPRECATED. Please update to version 3.0.0. ***
  
- *** Description: ***
- With this class you can draw a typical iOS badge indicator with a custom text on any view.
- Please use the allocator customBadgeWithString to create a new badge.
- In this version you can modfiy the color inside the badge (insetColor),
- the color of the frame (frameColor), the color of the text and you can
- tell the class if you want a frame around the badge.
+ The MIT License (MIT)
  
- *** License & Copyright ***
- Created by Sascha Marc Paulus www.spaulus.com on 04/2011. Version 2.0
- This tiny class can be used for free in private and commercial applications.
- Please feel free to modify, extend or distribution this class. 
- If you modify it: Please send me your modified version of the class.
- A commercial distribution of this class is not allowed.
+ Copyright (c) 2014 Sascha Paulus
  
- I would appreciate if you could refer to my website www.spaulus.com if you use this class.
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
  
- If you have any questions please feel free to contact me (open@spaulus.com).
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
  */
 
 
